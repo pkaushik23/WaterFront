5bb678d67c2132b6fdb0a22b857d5c5277e5b6c0
pkaushik23 <pkaushik@donotdisclose.com>
2020-06-09 23:45:43 +0530

    constants for application paths

---
+++ src/app/models/app.constants.ts
@@ -0,0 +1,18 @@
+interface ApplicationPathsType {
+    readonly DefaultLoginRedirectPath: string;
+    readonly Home: string;
+    readonly Dashboard: string;
+    readonly Profile: string;
+    readonly tools: string;
+}
+
+
+export const ApplicationPaths : ApplicationPathsType =  {
+    DefaultLoginRedirectPath: '',
+    Home: '/home',
+    Dashboard: '/dashboard',
+    Profile: '/profile',
+    tools: 'tools',
+};
+
+
